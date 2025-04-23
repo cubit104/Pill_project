fd488c67d7bf6e58ff7f779d83b035ff0373786f
cubit104 <72025422+cubit104@users.noreply.github.com>
2025-04-23 12:30:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -322,4 +322,4 @@ async def redirect_index2():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app,host="0.0.0.0",port=int(os.getenv("PORT",8000)))
+    uvicorn.run(app,host="0.0.0.0",port=int(os.getenv("PORT",10000)))
